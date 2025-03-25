860ffc82bad0e00ae407fae8d2ac35593b80d00d
The-Quan <145833348+The-Quan@users.noreply.github.com>
2025-03-25 22:22:49 +0700

    first commit

---
+++ src/main/java/org/example/callvideogroupspring/CallVideoGroupSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.callvideogroupspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallVideoGroupSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallVideoGroupSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/callvideogroupspring/CallVideoGroupSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.callvideogroupspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallVideoGroupSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
